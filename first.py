b367a7432d82597a183b4297287dcf0dfc45c1ba
misanthropicat <helgamogish@gmail.com>
2021-02-22 17:02:27 +0000

    * triangles recursively

--- first.py
+++ first.py
@@ -34,33 +34,59 @@ def get_style():
     fill(**monochrome)
 
 
-def get_position(start: Tuple[int, int], x_step: int, y_step: int):
-    st_width, st_height = start
-    return st_width + x_step, st_height + y_step
-
-
-def draw_recursively(func, x, y, h, w, x_step, y_step, h_step, w_step):
+def draw_recursively_2(func, x, y, h, w, x_step, y_step, h_step, w_step):
+    get_style()
     func(x, y, h, w)
     x += x_step
     y += y_step
     h *= h_step
     w *= w_step
-    if x == width or y == height:
+    if x >= width or y >= height or h >= height or w >= width:
         return
-    draw_recursively(func, x, y, h, w, x_step, y_step, h_step, w_step)
+    draw_recursively_2(func, x, y, h, w, x_step, y_step, h_step, w_step)
+
+
+def draw_recursively_3(func, x1, y1, z1, x2, y2, z2):
+    get_style()
+    func(x1, y1, z1, x2, y2, z2)
+    x1 /= 2
+    y1 /= 2
+    z1 /= 2
+    x2 /= 2
+    y2 /= 2
+    z2 /= 2
+    if x1 < 1 or y1 < 1 or z1 < 1:
+        return
+    draw_recursively_3(func, x1, y1, z1, x2, y2, z2)
 
 
 def draw():
-    x, y = width / 2, height / 2
-    h, w = 100, 100
+    monochrome = get_mono()
+    background(**monochrome)
+    x, y, z = width / 2, height / 2, 0
+    h, w = 50, 50
     x_step, y_step = 1, 1
-    h_step, w_step = 1.01, 1.01
-    draw_recursively(ellipse, x, y, h, w, x_step, y_step, h_step, w_step)
+    h_step, w_step = 1.05, 1.05
+    draw_recursively_2(ellipse, x, y, h, w, x_step, y_step, h_step, w_step)
+    color = get_color()
+    background(**color)
+    x, y = 0, height / 2
+    h, w = 10, 50
+    x_step, y_step = 10, 1
+    h_step, w_step = 1.03, 1.01
+    draw_recursively_2(rect, x, y, h, w, x_step, y_step, h_step, w_step)
+    monochrome = get_mono()
+    background(**monochrome)
+    color = get_color()
+    background(**color)
+    x1, y1, z1 = width, height, 50
+    x2, y2, z2 = width / 2, height / 2, 25
+    draw_recursively_3(triangle, x1, y1, z1, x2, y2, z2)
 
 
 def mouse_pressed():
     redraw()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     run(frame_rate=15)
